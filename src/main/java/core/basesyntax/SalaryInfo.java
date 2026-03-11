c85d075a8f20dfc82e019a96657f017a6af6b759
Olena Shtronda <e.shtronda@gmail.com>
2026-03-11 21:41:18 +0200

    remove hashmap and improve code according to the checklist

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,48 +2,54 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter);
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
 
-        StringBuilder builder = new StringBuilder();
-        String ls = System.lineSeparator();
-        builder.append("Report for period ")
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate from = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, DATE_FORMATTER);
+
+        StringBuilder reportBuilder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        reportBuilder.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
-                .append(ls);
+                .append(lineSeparator);
 
-        HashMap<String, Integer> dataMap = new HashMap<>();
-
-        for (String name : names) {
-            dataMap.put(name, 0);
-        }
+        int[] salaries = new int[names.length];
 
         for (String dataItem : data) {
             String[] dataItemParts = dataItem.split(" ");
-            LocalDate currentDate = LocalDate.parse(dataItemParts[0], formatter);
+            LocalDate currentDate = LocalDate.parse(dataItemParts[DATE_INDEX], DATE_FORMATTER);
 
             if (!currentDate.isBefore(from) && !currentDate.isAfter(to)) {
-                String name = dataItemParts[1];
-                int workingHours = Integer.parseInt(dataItemParts[2]);
-                int incomePerHour = Integer.parseInt(dataItemParts[3]);
-                int income = workingHours * incomePerHour;
-                dataMap.computeIfPresent(name, (k, v) -> v + income);
+                String name = dataItemParts[NAME_INDEX];
+                int hoursWorked = Integer.parseInt(dataItemParts[HOURS_INDEX]);
+                int hourlyRate = Integer.parseInt(dataItemParts[RATE_INDEX]);
+                int income = hoursWorked * hourlyRate;
+
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(name)) {
+                        salaries[i] += income;
+                        break;
+                    }
+                }
             }
         }
 
-        for (String name : names) {
-            builder.append(name)
+        for (int i = 0; i < names.length; i++) {
+            reportBuilder.append(names[i])
                     .append(" - ")
-                    .append(dataMap.get(name))
-                    .append(ls);
+                    .append(salaries[i])
+                    .append(lineSeparator);
         }
 
-        return builder.toString().trim();
+        return reportBuilder.toString().trim();
     }
 }
